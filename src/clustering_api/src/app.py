b2c53178f755baf4e64542c69ab0c5d227369596
makowskitomasz <t.makowski02@gmail.com>
2025-11-14 19:29:18 +0100

    feat: add NYC Taxi API router to the Clustering API

--- src/clustering_api/src/app.py
+++ src/clustering_api/src/app.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 
 from controllers.health_controller import health_api
 from controllers.stream_controller import router as stream_api
+from controllers.nyc_taxi_controller import router as nyc_taxi_api
+
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Clustering API")
     app.include_router(health_api)
     app.include_router(stream_api)
+    app.include_router(nyc_taxi_api)
     return app
